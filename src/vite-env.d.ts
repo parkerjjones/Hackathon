3b9acc2f01b9bf15fb35a614fca07d77b0e02a7a
Parker Jones <parkjjones@gmail.com>
2026-03-07 17:10:24 -0700

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_API_KEY: string
-  readonly VITE_CESIUM_ION_TOKEN?: string
+  readonly VITE_CESIUM_ION_TOKEN?: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyN2U1ZGYxZC1jY2NhLTRhMzctODAzYi0zYzBlZjFlOGE5YTIiLCJpZCI6NDAwMDA4LCJpYXQiOjE3NzI5MjU1Nzh9.Mq11O-s9BDYo1uaPGxpf_hUtj-TZsHo6EhFZhar3dbI
 }
 
 interface ImportMeta {
